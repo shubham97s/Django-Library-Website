a22ccb26464972d12eea86d80be1fcb885a6f60d
shubham97s <157363750+shubham97s@users.noreply.github.com>
2024-04-07 19:43:38 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,25 +1,13 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("add_book/", views.add_book, name="add_book"),
-    path("view_books/", views.view_books, name="view_books"),
-    path("view_students/", views.view_students, name="view_students"),
-    path("issue_book/", views.issue_book, name="issue_book"),
-    path("view_issued_book/", views.view_issued_book, name="view_issued_book"),
-    path("student_issued_books/", views.student_issued_books, name="student_issued_books"),
-    path("profile/", views.profile, name="profile"),
-    path("edit_profile/", views.edit_profile, name="edit_profile"),
+    path('admin/', admin.site.urls),
+    path('', include('library.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-    path("student_registration/", views.student_registration, name="student_registration"),
-    path("change_password/", views.change_password, name="change_password"),
-    path("student_login/", views.student_login, name="student_login"),
-    path("admin_login/", views.admin_login, name="admin_login"),
-    path("logout/", views.Logout, name="logout"),
-
-    path("delete_book/<int:myid>/", views.delete_book, name="delete_book"),
-    path("delete_student/<int:myid>/", views.delete_student, name="delete_student"),
-
-   
-]
